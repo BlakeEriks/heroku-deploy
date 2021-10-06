fe026eafacfed924c491fd1d3d4ecc49177dccf3
Blake Eriks <eblake98@vt.edu>
2021-10-05 15:08:06 -1000

    Commenting connection.js

--- models/connection.js
+++ models/connection.js
@@ -1,14 +1,17 @@
 /* Dependencies */
 const mongoose = require('mongoose')
 
+/* Mongo Connection Variables */
 const DB_URL = process.env.DATABASE_URL
 const DB_CONFIG = {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }
 
+/* Connect to MongoDB Cloud */
 mongoose.connect(DB_URL, DB_CONFIG)
 
+/* Connection logic */
 mongoose.connection
     .on('open', () => console.log('Connected to MongoDB Cloud'))
     .on('close', () => console.log('Disconnected from MongoDB Cloud'))
