72991791126a2fcaea1204d25ebed5f165601c13
Blake Eriks <eblake98@vt.edu>
2021-10-29 20:41:07 -1000

    Adding state to MovementCreate component

--- client/src/components/MovementCreate.js
+++ client/src/components/MovementCreate.js
@@ -1,17 +1,23 @@
+import { useState } from "react"
+
 const MovementCreate = () => {
+
+    const [movement, setMovement] = useState({
+        type: '',
+        sets: []
+    })
+
     return (
         <form>
             <fieldset className="movement-info">
                 <legend className="highlight">Create a Movement</legend>
                 <div className="movement-label">Movement Type:</div>
-                <input type="text" name="type" className="movement-input"></input>
+                <input type="text" name="type" value={movement.type} className="movement-input" onChange={event => setMovement({...movement, type:event.target.value})}></input>
                 <div className="movement-label">Sets:</div>
-                <template>
-                    <div>
-                        <input type="text" name="weight[]" placeholder="0" className="set-input" /> lbs
-                        <input type="text" name="reps[]" placeholder="0" className="set-input" /> reps
-                    </div>
-                </template>
+                <div>
+                    <input type="text" name="weight" placeholder="0" className="set-input" /> lbs
+                    <input type="text" name="reps" placeholder="0" className="set-input" /> reps
+                </div>
                 <button type="button" className="pink-on-white">+ Add Set</button>
             </fieldset>
             <button className="add-movement">Create Movement</button>
