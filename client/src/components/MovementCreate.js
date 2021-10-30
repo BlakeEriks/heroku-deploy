4a9518d49fd0b67c55f315ead9da69749b909ed5
Blake Eriks <eblake98@vt.edu>
2021-10-29 21:31:58 -1000

    Adding more state to MovementCreate component

--- client/src/components/MovementCreate.js
+++ client/src/components/MovementCreate.js
@@ -2,25 +2,42 @@ import { useState } from "react"
 
 const MovementCreate = () => {
 
-    const [movement, setMovement] = useState({
-        type: '',
-        sets: []
-    })
+    const [movementType, setMovementType] = useState('')
+
+    const [sets, setSets] = useState([]);
+
+    const updateSet = index => event => {
+        let newSets = [...sets]
+        newSets[index][event.target.name] = event.target.value;
+        setSets([...newSets])
+    }
+
+    const onSubmit = event => {
+        event.preventDefault()
+        let movement = {type: movementType, sets}
+        console.log(movement)
+    }
 
     return (
-        <form>
+        <form onSubmit={onSubmit}>
             <fieldset className="movement-info">
                 <legend className="highlight">Create a Movement</legend>
                 <div className="movement-label">Movement Type:</div>
-                <input type="text" name="type" value={movement.type} className="movement-input" onChange={event => setMovement({...movement, type:event.target.value})}></input>
+                <input type="text" name="type" value={movementType} className="movement-input" onChange={event => setMovementType(event.target.value)}></input>
                 <div className="movement-label">Sets:</div>
-                <div>
+                {sets.map( (set, index) => 
+                    <div key={index}>
+                        <input type="text" name="weight" value={set.weight} className="set-input" onChange={updateSet(index)}/> lbs
+                        <input type="text" name="reps" value={set.reps} className="set-input" onChange={updateSet(index)}/> reps
+                    </div>
+                )}
+                {/* <div>
                     <input type="text" name="weight" placeholder="0" className="set-input" /> lbs
                     <input type="text" name="reps" placeholder="0" className="set-input" /> reps
-                </div>
-                <button type="button" className="pink-on-white">+ Add Set</button>
+                </div> */}
+                <button type="button" className="pink-on-white" onClick={() => setSets([...sets, {reps: '', weight: ''}])}>+ Add Set</button>
             </fieldset>
-            <button className="add-movement">Create Movement</button>
+            <button type="submit" className="add-movement">Create Movement</button>
         </form>
     )
 }
