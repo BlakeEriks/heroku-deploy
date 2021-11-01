58d42c5c20ee1e4351a4fd6ed71d89174d46baf9
Blake Eriks <eblake98@vt.edu>
2021-10-31 19:39:15 -1000

    Change delete route

--- controllers/movement.js
+++ controllers/movement.js
@@ -15,8 +15,22 @@ movementRouter.get('/new', (req,res) => {
 })
 
 movementRouter.delete('/:id', (req,res) => {
-    Movement.findByIdAndDelete(req.params.id, (err, movement) => {
-        res.redirect(`/movements?liftId=${movement.lift_id}`)
+    Movement.findByIdAndDelete(req.params.id)
+        .then(data => {
+            if (!data) {
+                res.status(404).send({
+                    message: `Cannot delete Tutorial with id=${id}. Maybe Tutorial was not found!`
+                })
+            } else {
+                res.send({
+                    message: "Tutorial was deleted successfully!"
+                })
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Could not delete Tutorial with id=" + id
+            })
     })
 })
 
