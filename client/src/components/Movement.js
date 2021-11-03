f6b6573fe26f279e71a6d8fca90f62aa87910b3b
Blake Eriks <eblake98@vt.edu>
2021-11-02 15:08:12 -1000

    Remove unused a tag

--- client/src/components/Movement.js
+++ client/src/components/Movement.js
@@ -2,9 +2,7 @@ const Movement = ({type, sets, _id, deleteMovement}) => {
 
     return (
         <div className="movement-item" >
-            <a>
             {type} - {sets?.length} Sets 
-            </a>
             <button type="button" className="movement-delete" onClick={() => deleteMovement(_id)} >
                 <i className="fas fa-times"></i>
             </button>
