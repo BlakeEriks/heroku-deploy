e2547c082d90bc97270c2aa20b6b766ff2aac900
Blake Eriks <eblake98@vt.edu>
2021-11-02 21:21:15 -1000

    Layout and adding useeffect to change mode on date change

--- client/src/components/LiftEditor.js
+++ client/src/components/LiftEditor.js
@@ -3,10 +3,16 @@ import moment from 'moment'
 import MovementCreate from './MovementCreate'
 import Movement from './Movement'
 import { AddOutline } from 'react-ionicons'
+import { useEffect } from 'react/cjs/react.development'
+import AnimateHeight from 'react-animate-height';
 
 const LiftEditor = ({movements, selectedDate, createMovement, deleteMovement}) => {
 
     const [mode, setMode] = useState('show')
+
+    useEffect( () => {
+        setMode('show')
+    }, [selectedDate])
 
     return (
         <div className="lift-editor-panel">
@@ -14,25 +20,27 @@ const LiftEditor = ({movements, selectedDate, createMovement, deleteMovement}) =
                 <h1 className="notepad-title">
                     {moment(selectedDate).format('MMM Do, YYYY')}
                 </h1>
-                <div className="notepad-content">
+                <div className="notepad-content" >
                     {mode === 'show'
                     ?
                     <>
-                    {movements && movements.map( movement => 
+                    {movements ? movements.map( movement => 
                         <Movement key={movement._id} {...movement} deleteMovement={deleteMovement} />
-                    )}
-                    <button onClick={() => setMode('create')} className='movement-add'>
-                        <AddOutline 
-                            color={'#FFFFFF'}
-                            height='25px'
-                            width='25px'
-                            title={'Add Movement'}
-                        />
-                    </button>
+                    ) : <div className='movement-item'>No Movements</div>}
                     </>
                     :
                     <MovementCreate createMovement={createMovement} setMode={setMode}/>
                     }
+                </div>
+                <div className="notepad-footer">
+                    <button onClick={() => setMode('create')} className='movement-add'>
+                            <AddOutline 
+                                color={'#d00054'}
+                                height='35px'
+                                width='35px'
+                                title={'Add Movement'}
+                            />
+                    </button>
                 </div>
             </div>
         </div>
