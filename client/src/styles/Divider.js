fcb0642e516f9d912d37859be9121fa536e63c17
Blake Eriks <eblake98@vt.edu>
2021-11-03 22:15:58 -1000

    Add divider styled component

---
+++ client/src/styles/Divider.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+import Colors from './Colors'
+
+export const HorizontalDivider = styled.div`
+    border: 2px solid ${Colors.accent};
+    width: 90%;
+    text-align: center;
+`
