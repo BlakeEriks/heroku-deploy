c76efeb6878bf9e5b8563acd9d010a1375f828d4
Blake Eriks <eblake98@vt.edu>
2021-11-03 22:14:54 -1000

    Import styled components

--- client/src/components/Movement.js
+++ client/src/components/Movement.js
@@ -1,11 +1,14 @@
+import { SquareButton } from "../styles/Button"
+import { TimesIcon } from "../styles/Icon"
+
 const Movement = ({type, sets, _id, deleteMovement}) => {
 
     return (
         <div className="movement-item" >
             {type} - {sets?.length} Sets 
-            <button type="button" className="movement-delete" onClick={() => deleteMovement(_id)} >
-                <i className="fas fa-times"></i>
-            </button>
+            <SquareButton onClick={() => deleteMovement(_id)}>
+                <TimesIcon />
+            </SquareButton>
         </div>
     )
 }
