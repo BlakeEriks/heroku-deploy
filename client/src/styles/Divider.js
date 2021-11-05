77df156eee7ee2d1f012e107575734fa02201f08
Blake Eriks <eblake98@vt.edu>
2021-11-04 22:00:37 -1000

    Add vertical divider to divider styled components

--- client/src/styles/Divider.js
+++ client/src/styles/Divider.js
@@ -2,7 +2,13 @@ import styled from 'styled-components'
 import Colors from './Colors'
 
 export const HorizontalDivider = styled.div`
-    border: 2px solid ${props => Colors[props.color] || Colors.accent};
+    border: ${props => props.width || '2px'} solid ${props => Colors[props.color] || Colors.accent};
     width: 90%;
     margin: 0 auto;
+`
+
+export const VerticalDivider = styled.div`
+    border: ${props => props.width || '2px'} solid ${props => Colors[props.color] || Colors.accent};
+    height: ${props => props.height || '90%'};
+    margin: auto 0;
 `
