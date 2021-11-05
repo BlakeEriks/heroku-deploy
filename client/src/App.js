fe4a254570c76b48e4911cdb01f3d10c0f842606
Blake Eriks <eblake98@vt.edu>
2021-11-04 21:58:38 -1000

    Add Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
-import LiftMate from './components/LiftMate';
+import LogBook from './components/LogBook';
+import {Routes, Route} from 'react-router-dom'
+import TabNav from './components/TabNav';
+import Navbar from './components/Navbar';
+import Trends from './components/Trends';
+import Home from './components/Home';
 
 function App() {
   return (
-    <LiftMate />
+    <>
+      <Navbar />
+      <TabNav />
+      <Routes>
+        <Route path="/home" element={<Home />}/>
+        <Route path="/logbook" element={<LogBook />}/>
+        <Route path="/trends" element={<Trends />}/>
+      </Routes>
+    </>
   )
 }
 
